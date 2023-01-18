6f9d1f256dfe00b378a18fa3f39c2932c8717de8
rizky14045 <mrizkysaputra17@gmail.com>
2023-01-18 21:21:23 +0700

    change title to pln

--- resources/views/front/pages/login.blade.php
+++ resources/views/front/pages/login.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login Form PJB Request</title>
+	<title>Login Form</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
